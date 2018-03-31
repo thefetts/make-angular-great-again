cbd6c0f9fbd4c0f3c68388c3ebc43e41d40cd40d
Jordan Fallon <thefetts@gmail.com>
2018-03-30 18:33:21 -0600

    Let's get dynamic!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  roster: Member[] = [
+    {title: 'Lord Commander', name: 'Jeor Mormont', leader: true, ordinal: 997},
+    {title: 'Maester', name: 'Aemon Targaryen', leader: false},
+    {title: 'First Ranger', name: 'Benjen Stark', leader: false},
+    {title: 'First Steward', name: 'Bowen Marsh', leader: false},
+    {title: 'Master-at-arms', name: 'Ser Alliser Thorne', leader: false},
+    {title: 'First Builder', name: 'Othell Yarwyck', leader: false}
+  ]
+
+  getLeaderTitle(): string {
+    const leader = this.roster.find(member => member.leader)
+    return `${this.suffix(leader.ordinal)} ${leader.title} ${leader.name}`
+  }
+
+  private suffix(num: number): string {
+    const ones = num % 10, tens = num % 100
+    if (ones === 1 && tens !== 11) return `${num}st`
+    if (ones === 2 && tens !== 12) return `${num}nd`
+    if (ones === 3 && tens !== 13) return `${num}rd`
+    return `${num}th`
+  }
 }
---
+++ src/app/member.model.ts
@@ -0,0 +1,6 @@
+interface Member {
+  title: string
+  name: string
+  leader: boolean
+  ordinal?: number
+}
